ea14c30a313c8b85115373ed4fffe5daddee407f
Diego Pereira Grassato <diego.grassato@bradesco.com.br>
2022-11-09 11:46:58 -0300

    Adiciona evento para o react-native

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import './App.css';
-import Mapa from './Mapa';
+import * as React from 'react';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+  
+  const handleClick = () => {
+   setCount(count + 1);
+   if (window.ReactNativeWebView) {
+      window.ReactNativeWebView.postMessage(JSON.stringify({ type: "token", message: "your message" , data: count}));
+   }
+  };
+  
+  const handleClickZero = () => {
+   setCount(0);
+   if (window.ReactNativeWebView) {
+      window.ReactNativeWebView.postMessage(JSON.stringify({ type: "token", message: "your message" , data: 0}));
+   }
+  };
   return (
     <div className="App">
       <header className="App-header">
          
-        <Mapa /> 
+        <div>
+         <button type="button" onClick={handleClick}>
+          Click Me
+        </button>
+
+        <button type="button" onClick={handleClickZero}>
+         Zerar
+        </button>
+
+        {count}
+      </div>
       </header>
     </div>
   );
